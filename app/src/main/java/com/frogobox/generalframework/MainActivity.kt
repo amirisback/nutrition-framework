8030343425b383f391e17b008434d823f3afe048
faisalamir <faisalamircs@gmail.com>
2021-04-27 08:33:48 +0700

    add: sample recycler view

--- app/src/main/java/com/frogobox/generalframework/MainActivity.kt
+++ app/src/main/java/com/frogobox/generalframework/MainActivity.kt
@@ -2,10 +2,57 @@ package com.frogobox.generalframework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import com.frogobox.generalframework.databinding.ActivityMainBinding
+import com.frogobox.recycler.core.IFrogoViewAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+
+        binding.apply {
+
+            frogoRv.injector<String>()
+                    .addData(name())
+                    .addCustomView(R.layout.frogo_rv_list_type_1)
+                    .addCallback(object: IFrogoViewAdapter<String>{
+                        override fun onItemClicked(data: String) {
+
+                        }
+
+                        override fun onItemLongClicked(data: String) {
+                            // TODO("Not yet implemented")
+                        }
+
+                        override fun setupInitComponent(view: View, data: String) {
+                            val textRV = view.findViewById<TextView>(R.id.frogo_rv_list_type_1_tv_title)
+                            textRV.text = data
+                        }
+                    })
+                    .createLayoutLinearVertical(false)
+                    .build()
+
+        }
+
+
     }
+
+
+    private fun name() : MutableList<String> {
+        val listData = mutableListOf<String>()
+        listData.add("Aplikasi Nutrisi")
+        listData.add("Aplikasi Nutrisi")
+        listData.add("Aplikasi Nutrisi")
+        listData.add("Aplikasi Nutrisi")
+        return listData
+    }
+
 }
