8a53ce13a418c11cadace5eca2a89b7b7e40144b
Muhammad Faisal Amir <faisalamircs@gmail.com>
2021-08-19 15:00:02 +0700

    add: Framework Module

---
+++ framework/src/main/java/com/frogobox/framework/data/NutritionData.kt
@@ -0,0 +1,16 @@
+package com.frogobox.framework.data
+
+/*
+ * Created by faisalamir on 19/08/21
+ * NutritionFramework
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * -----------------------------------------
+ * Copyright (C) 2021 FrogoBox Inc.      
+ * All rights reserved
+ *
+ */
+class NutritionData {
+}
