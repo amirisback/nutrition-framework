54985b6c2cfdc517cfd90a310f1be71fcf36c225
Muhammad Faisal Amir <faisalamircs@gmail.com>
2021-10-19 13:54:29 +0700

    add: resource ui

--- nutritionframework/build.gradle.kts
+++ nutritionframework/build.gradle.kts
@@ -45,14 +45,15 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("com.google.android.material:material:1.4.0")
-
     implementation(compose.ui)
     implementation(compose.runtime)
     implementation(compose.material)
     implementation(compose.materialIconsExtended)
+
+    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.1")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
