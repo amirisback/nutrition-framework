39c113309325f1141d98f2440e5548882fe4e362
Muhammad Faisal Amir <faisalamircs@gmail.com>
2021-11-20 18:05:32 +0700

    add: grid item files

---
+++ nutritioncore/src/main/java/com/frogobox/nutritioncore/compose/widget/NutriGridItemCompose.kt
@@ -0,0 +1,16 @@
+package com.frogobox.nutritioncore.compose.widget
+
+
+/*
+ * Created by faisalamir on 20/11/21
+ * NutritionFramework
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * -----------------------------------------
+ * Copyright (C) 2021 FrogoBox Inc.      
+ * All rights reserved
+ *
+ */
+
