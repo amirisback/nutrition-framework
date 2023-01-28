e7d102622e6aac67fedf1414a74fe4ab705b04f3
amirisback <faisalamircs@gmail.com>
2023-01-28 13:17:46 +0700

    update: Manifest Post Notification permission

--- nutritionapp/src/main/java/com/frogobox/nutritionapp/mvvm/androidmethod/notification/ManualActivity.kt
+++ nutritionapp/src/main/java/com/frogobox/nutritionapp/mvvm/androidmethod/notification/ManualActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.widget.RemoteViews
 import android.content.Intent
 import android.app.PendingIntent
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.frogobox.nutritionapp.NutriApplication
@@ -57,6 +59,20 @@ class ManualActivity : BaseActivity<ActivityManualBinding>() {
             .setCustomBigContentView(expandedView) //.setStyle(new NotificationCompat.DecoratedCustomViewStyle())
             .build()
 
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                "android.permission.POST_NOTIFICATIONS"
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
         notificationManager.notify(NutriApplication.NOTIFICATION_ID, notification)
     }
 
